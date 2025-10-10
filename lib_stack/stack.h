9fbca3018b0e6379ed8a00743f8fc0cd4182cd5c
Пользователь <olesyabogdaniha2015@gmail.com>
2025-10-10 18:35:20 +0300

    added Stack with tests

---
+++ lib_stack/stack.h
@@ -0,0 +1,80 @@
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+#include "../lib_TVector/tvector.h"
+
+template <class T>
+class Stack {
+    T* _data;
+    int _top = -1;
+    int _size;
+
+public:
+    Stack();
+    Stack(int);
+    Stack(const Stack&);
+
+    void push(int);
+    void pop();
+    int top() const;
+    inline bool is_empty() const noexcept {
+        return _top == -1;
+    }
+    inline bool is_full() const noexcept {
+        return _top == _size - 1;
+    }
+    void clear() noexcept;
+};
+
+template <class T>
+Stack<T>::Stack() {
+    _size = 15;
+    _data = new T[_size];
+}
+
+template <class T>
+Stack<T>::Stack(int size) {
+    if (size <= 0)
+        throw std::invalid_argument("Size of stack must be > 0");
+    _size = size;
+    _data = new T[_size];
+}
+
+template <class T>
+Stack<T>::Stack(const Stack& other) {
+    _size = other._size;
+    _top = other._top;
+    _data = new T[_size];
+    for (int i = 0; i < _size; i++)
+        _data[i] = other._data[i];
+}
+
+template <class T>
+void Stack<T>::push(int value) {
+    if (is_full())
+        throw std::logic_error("The stack is full");
+
+    _data[++_top] = value;
+}
+
+template <class T>
+void Stack<T>::pop() {
+    if (is_empty())
+        throw std::logic_error("The stack is empty");
+
+    _top--;
+}
+
+template <class T>
+int Stack<T>::top() const {
+    if (is_empty())
+        throw std::logic_error("The stack is empty");
+
+    return _data[_top];
+}
+
+template <class T>
+void Stack<T>::clear() noexcept {
+    _top = -1;
+}
+
+#endif // !STACK_STACK_H
---
+++ tests/test_stack.cpp
@@ -0,0 +1,83 @@
+#include <gtest/gtest.h>
+#include "stack.h"
+
+TEST(TestStackLib, test_defualt_constructor) {
+    Stack<int> stack;
+}
+
+TEST(TestStackLib, test_initialize_constructor) {
+    ASSERT_NO_THROW(Stack<int> stack(5));
+}
+
+TEST(TestStackLib, test_initialize_constructor_with_negative_size) {
+    ASSERT_ANY_THROW(Stack<int> stack(-5));
+}
+
+TEST(TestStackLib, test_copy_constructor) {
+    Stack<int> stack1(3);
+    ASSERT_NO_THROW(Stack<int> stack2(stack1));
+}
+
+TEST(TestStackLib, test_is_empty_if_empty) {
+    Stack<int> stack(5);
+    ASSERT_TRUE(stack.is_empty());
+}
+
+TEST(TestStackLib, test_push_and_is_empty_if_not) {
+    Stack<int> stack(5);
+    stack.push(3);
+    ASSERT_FALSE(stack.is_empty());
+}
+
+TEST(TestStackLib, test_push_if_full) {
+    Stack<int> stack(5);
+    for (int i = 0; i < 5; i++)
+        stack.push(3);
+    ASSERT_ANY_THROW(stack.push(3));
+}
+
+TEST(TestStackLib, test_is_full_if_not) {
+    Stack<int> stack(5);
+    stack.push(3);
+    ASSERT_FALSE(stack.is_full());
+}
+
+TEST(TestStackLib, test_is_full_if_full) {
+    Stack<int> stack(5);
+    for (int i = 0; i < 5; i++)
+        stack.push(3);
+    ASSERT_TRUE(stack.is_full());
+}
+
+TEST(TestStackLib, test_top) {
+    Stack<int> stack(5);
+    for (int i = 0; i < 4; i++)
+        stack.push(i + 1);
+    ASSERT_TRUE(stack.top(), 4);;
+}
+
+TEST(TestStackLib, test_top_if_empty) {
+    Stack<int> stack(5);
+    ASSERT_ANY_THROW(stack.pop());
+}
+
+TEST(TestStackLib, test_pop) {
+    Stack<int> stack(5);
+    for (int i = 0; i < 4; i++)
+        stack.push(i + 1);
+    stack.pop();
+    ASSERT_TRUE(stack.top(), 3);;
+}
+
+TEST(TestStackLib, test_pop_if_empty) {
+    Stack<int> stack(5);
+    ASSERT_ANY_THROW(stack.pop());
+}
+
+TEST(TestStackLib, test_clear) {
+    Stack<int> stack(5);
+    for (int i = 0; i < 4; i++)
+        stack.push(i + 1);
+    stack.clear();
+    ASSERT_TRUE(stack.is_empty());
+}
