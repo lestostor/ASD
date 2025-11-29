7989df1616b0255395b81da07577a1c055c59b5e
Пользователь <olesyabogdaniha2015@gmail.com>
2025-11-29 15:51:00 +0300

    fixed dsu

--- lib_dsu/dsu.cpp
+++ lib_dsu/dsu.cpp
@@ -5,7 +5,7 @@ DSU::DSU(size_t size) {
     _size = size;
     _parent = new int[size];
     _rank = new int[size];
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         _parent[i] = i;
         _rank[i] = 0;
     }
@@ -16,16 +16,16 @@ DSU::~DSU() {
     delete[]_rank;
 }
 
-int DSU::find(int x) const {
+int DSU::find(int x) {
     if (x >= _size || x < 0)
         throw std::invalid_argument("Wrong element");
 
-    return find_rec(x);
+    return _parent[x] = find_rec(_parent[x]);
 }
 
-int DSU::find_rec(int x) const {
+int DSU::find_rec(int x) {
     if (x == _parent[x]) return x;
-    return find_rec(_parent[x]);
+    return _parent[x] = find_rec(_parent[x]);
 }
 
 void DSU::unite(int x, int y) {
@@ -38,7 +38,7 @@ void DSU::unite(int x, int y) {
 
     if (_rank[parent_x] >= _rank[parent_y])
         _parent[parent_y] = parent_x;
-    else if (_rank[parent_y] > _rank[parent_x])
+    else
         _parent[parent_x] = parent_y;
     if (_rank[parent_x] == _rank[parent_y])
         _rank[parent_x]++;
